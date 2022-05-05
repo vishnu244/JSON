6d78a228bda219bbc193101daa9f5b828fe3a1f8
vishnu244 <vishnunali244@gmail.com>
2022-05-05 17:31:11 +0530

    [vishnu] inventory management

---
+++ Json/Program.cs
@@ -0,0 +1,21 @@
+﻿using Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Welcome to JSON!");
+
+string file = @"C:\Users\Admin\Desktop\Vishnu\JSON\Json\Inventorydata.json";
+List<inventory> files = JsonSerializer.Deserialize<List<inventory>>(File.ReadAllText(file));
+foreach (var data in files)
+{
+    Console.WriteLine("name: " + data.Name + " weight: " + data.Weight + " price: "+ data.Price);
+}
+
+string json = JsonSerializer.Serialize(files);
+Console.WriteLine("Serialized Json Object ");
+Console.WriteLine(json + "\n");
+
+
+
